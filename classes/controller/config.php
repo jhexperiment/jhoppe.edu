ac824ccfc97faf6163220f453a7a5ee32f7f7bf4
John Hauge <jhxmonkey@jhs-macbook-pro.local>
2011-04-28 13:21:12 -1000

    tweak

--- classes/controller/config.php
+++ classes/controller/config.php
@@ -13,7 +13,7 @@ class Controller_Config extends Controller_Template
 	public function action_index()
 	{
 		if ( ! $_SESSION['JH_LOGGED_IN']) {
-			Request::instance()->redirect("/login");
+			Request::instance()->redirect("/");
 		}
 		$this->init();
 		
